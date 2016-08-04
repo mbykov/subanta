6b0b444b18a5849f80c7cad9b474904821f50b30
Michael Bykov <m.bykov@gmail.com>
2016-08-04 17:31:40 +0300

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+// subanta.js
+
+var fs = require('fs');
+var util = require('util');
+var _ = require('underscore');
+// var slp = require('../sandhi/slp');
+var path = require('path');
+var s = require('sandhi');
+var c = s.const;
+var u = s.u;
+var sandhi = s.sandhi;
+var inc = u.include;
+var log = u.log;
+var p = u.p;
+// var salita = require('salita-component');
+// var sha1 = require('sha1');
+
+// var dataPath = path.join(__dirname, '../uohyd/drpatel/generatedverbforms_deva20062016.csv');
+
+var pars = ['तिप्', 'तस्', 'झि', 'सिप्', 'थस्', 'थ', 'मिप्', 'वस्', 'मस्'];
+var atms = ['त', 'आताम्', 'झ', 'थास्', 'आथाम्', 'ध्वम्', 'इट्', 'वहि', 'महिङ्'];
+
+
+exports = module.exports = stemmer();
+
+function stemmer() {
+    if (!(this instanceof stemmer)) return new stemmer();
+}
+
+
+
+// =========================================== QUERY
+
+// переименовать в find, и в run.js тоже
+stemmer.prototype.query = function(query, sups) {
+    log('SUP', sups[0]);
+    return [];
+}
---
+++ run.js
@@ -0,0 +1,62 @@
+/*
+  node run.js eva
+*/
+
+var lat = process.argv.slice(2)[0] || false;
+var find = process.argv.slice(3)[0] || false;
+
+var _ = require('underscore');
+var util = require('util');
+var salita = require('salita-component');
+
+var path = require('path');
+var fs = require('fs');
+
+var sup = require('./lib/sup');
+var subanta = require('./index');
+var s = require('sandhi');
+// var c = s.const;
+var u = s.u;
+// var sandhi = s.sandhi;
+var log = u.log;
+var p = u.p;
+
+if (!lat) return log('?');
+
+var form;
+if (/[a-zA-Z0-1]/.test(lat[0])) {
+    form = salita.slp2sa(lat);
+} else {
+    form = lat;
+    lat = salita.sa2slp(form);
+}
+
+if (find) log('subanta find:', lat, form); // लोकृ्-लोक् // लोचृ्-लोच्// प्-पा
+else log('subanta parse:', lat, form);
+
+var eu =  ['sg.nom', 'sg.acc', 'sg.ins', 'sg.dat', 'sg.abl', 'sg.gen', 'sg.loc', 'sg.voc', 'du.nom', 'du.acc', 'du.ins', 'du.dat', 'du.abl', 'du.gen', 'du.loc', 'du.voc', 'pl.nom', 'pl.acc', 'pl.ins', 'pl.dat', 'pl.abl', 'pl.gen', 'pl.loc', 'pl.voc'];
+var hi =  ['सु', 'औ', 'जस्', 'अम्', 'औट्', 'शस्', 'टा', 'भ्याम्', 'भिस्', 'ङे', 'भ्याम्', 'भ्यस्', 'ङसि', 'भ्याम्', 'भ्यस्', 'ङस्', 'ओस्', 'आम्', 'ङि', 'ओस्', 'सुप्'];
+
+
+var sups = [];
+
+for (var gend in sup) {
+    var affs = sup[gend];
+    // log(num, affix);
+    for (var aff in affs) {
+        var terms = affs[aff];
+        var dict = terms['dict'];
+        log(gend, aff, dict);
+    }
+}
+
+
+console.time("queryTime");
+
+var queries = subanta.query(form, sups);
+
+// ==============
+p(queries);
+log('qs size:', queries.length);
+
+console.timeEnd("queryTime");
